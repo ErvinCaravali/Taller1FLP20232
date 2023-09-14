c0021c54c957f5221bc1d77d8b0c550b0247a5ed
estiradikal <estivenfrv@gmail.com>
2023-09-14 15:52:27 -0500

    Puntos actualizados 1,4,7,10,13,16

--- ejercicios-taller1.rkt
+++ ejercicios-taller1.rkt
@@ -10,41 +10,42 @@
 ;;Proposito
 ;; X x Y -> Y' X' Procedimiento que recibe en una lista, y devuelve una lista similar con los pares ordenados invertidos
 ;;
-;;Gram�tica BNF:
+;;Gramatica BNF:
 ;;<lista> :=()
 ;;         |(<valor-de-scheme> <lista>)
 ;;
 ;;Pruebas
-;;(display (invert '(("es" "racket" "A") ("genial" "muy") (17 29) (81 o))))
-;;(display (invert '((5 9) (10 91) (82 7) (a e) ("hola" "Mundo" "MiMundoHola"))))
-;;(display (invert '((a 1) (a 2) (1 b) (2 b))))
-;;(display (invert '((a c) (d f) (g i) (j L))))
-;;(display (invert '((1 2) (1 2) (1 1) (0 1))))
+;;(invert '((a 1) (a 2) (1 b) (2 b))); Deberia imprimir ((1 a) (2 a) (b 1) (b 2))
+;;(invert '((5 9) (10 91) (82 7) (a e) ("hola" "Mundo"))); Deberia imprimir ((9 5) (91 10) (7 82) (e a) ("Mundo" "hola"))
+;;(invert '(("es" "racket") ("genial" "muy") (17 29) (81 o))); Deberia imprimir (("racket" "es") ("muy" "genial") (29 17) (o 81))
+;;(invert '((a z) (b z) (z r))); Deberia imprimir ((z a) (z b) (r z))
+;;(invert '(("Hola" "Mundo" "Esto sobra") (1,2,3) (c b a) (b a) (a b)));Deberia imprimir ((a b) (b a))
+
 
 (define (invert lista)
-  ; Funci�n auxiliar para invertir un par (x, y) a (y, x)
-  (define (invert-pair pair)
-    (list (cadr pair) (car pair)))
+  ; Función auxiliar para invertir un par (x, y) a (y, x)
+  (define invert-pair
+    (lambda (pair)
+      (list (cadr pair) (car pair))))
 
-  ; Funci�n recursiva para invertir una lista de pares
-  (define (invert-list lista acumulador)
-    (cond
-      [(null? lista) acumulador] ; Si la lista est� vac�a, devuelve el acumulador
-      [else
-       (let ((elemento (car lista)))
-         (if (= 2 (length elemento))
-             (invert-list (cdr lista) (cons (invert-pair elemento) acumulador))
-             (begin
-               (display "La lista ")
-               (display elemento)
-               (display " no es de tama�o 2")
-               (newline)
-               (invert-list (cdr lista) acumulador))))]))
+  ; Función recursiva para invertir una lista de pares
+  (define invert-list
+    (lambda (lista acumulador)
+      (cond
+        [(null? lista) acumulador] ; Si la lista está vacía, devuelve el acumulador
+        [else
+         (let ((elemento (car lista)))
+           (if (= 2 (length elemento))
+               (invert-list (cdr lista) (cons (invert-pair elemento) acumulador))
+               (begin
+                 (display "La lista ")
+                 (display elemento)
+                 (display " no es de tamaño 2")
+                 (newline)
+                 (invert-list (cdr lista) acumulador))))])))
 
-  ; Invierte la lista utilizando la funci�n recursiva invert-list
+  ; Invierte la lista utilizando la función recursiva invert-list
   (reverse (invert-list lista '())))
-
-
 
 ;;3
 ;; list-set:
@@ -76,47 +77,48 @@
 ;;filter-in
 ;; 4.
 ;;Proposito
-;; lst x [L] x (P) -> [L'] => P'  Est� funci�n filtra los elementos de la lista original seg�n el criterio proporcionado por el
+;; [L] x (P) -> [L'] => P'  Esta funcion filtra los elementos de la lista original segun el criterio proporcionado por el
 ;;predicado y devuelve una lista con esos elementos que cumplen con ese criterio.
 ;;
-;;Gram�tica BNF:
+;;Gramatica BNF:
 ;;<lista> :=()
 ;;         |(<valor-de-scheme> <lista>)
 ;;
 ;;Pruebas
-;;(display (filter-in number? '(a 2 (1 3) b 7))) ; Filtra n�meros
-;(display (filter-in symbol? '(a (b c) 17 foo))) ; Filtra s�mbolos
-;;(display (filter-in string? '(a b u "univalle" "racket" "flp" 28 90 (1 2 3)))) ; Filtra cadenas
-;;(display (filter-in number? '(a 1 (1 9) c 1)))
-;;(display (filter-in symbol? '(a b 123 "hola" 'c d 'mundo)))
+;;(filter-in number? '(a 2 (1 3) b 7));Deberia imprimir (2 7)
+;;(filter-in symbol? '(a (b c) 17 foo));Deberia imprimir (a foo)
+;;(filter-in string? '(a b u "univalle" "racket" "flp" 28 90 (1 2 3)));Deberia imprimir ("univalle" "racket" "flp")
+;;(filter-in symbol? '(a 1 2 3 (9 8 7) b 9 8));Deberia imprimir (a b)
+;;(filter-in string? '(a "B" 1 "13" (1 2))); Deberia imprimir ("B" "13")
 
-; Definici�n de una funci�n personalizada my-car para obtener el primer elemento de una lista
-(define (my-car lst)
-  (if (null? lst)
-      ( "La lista est� vac�a")
-      (car lst)))
-
-; Definici�n de una funci�n personalizada my-cdr para obtener el resto de una lista
-(define (my-cdr lst)
-  (if (null? lst)
-      ( "La lista est� vac�a")
-      (cdr lst)))
-
-; Definici�n de la funci�n filter-in que toma un predicado P y una lista L como argumentos
 (define (filter-in P L)
-  ; Definici�n de una funci�n auxiliar llamada filter-helper
-  (define (filter-helper P L result)
-    (cond
-      ; Si la lista est� vac�a, devuelve el resultado invertido
-      [(null? L) (reverse result)]
-      ; Si el primer elemento cumple con el predicado, cons�rvalo y sigue filtrando el resto de la lista
-      [(P (my-car L)) (filter-helper P (my-cdr L) (cons (my-car L) result))]
-      ; Si el primer elemento no cumple con el predicado, om�telo y sigue filtrando el resto de la lista
-      [else (filter-helper P (my-cdr L) result)]))
+  ; Función auxiliar filter-helper utilizando cálculo lambda y recursión
+  (define filter-helper
+    (lambda (P L result)
+      (cond
+        ; Si la lista está vacía, devuelve el resultado invertido
+        ((null? L) (reverse result))
+        ; Si el primer elemento cumple con el predicado, consérvalo y sigue filtrando el resto de la lista
+        ((P (my-car L)) (filter-helper P (my-cdr L) (cons (my-car L) result)))
+        ; Si el primer elemento no cumple con el predicado, omítelo y sigue filtrando el resto de la lista
+        (else (filter-helper P (my-cdr L) result)))))
 
-  ; Inicializa el resultado como una lista vac�a y llama a la funci�n auxiliar
+  ; Inicializa el resultado como una lista vacía y llama a la función auxiliar
   (filter-helper P L '()))
 
+; Definición de la función personalizada my-car para obtener el primer elemento de una lista
+(define my-car
+  (lambda (lst)
+    (if (null? lst)
+        "La lista está vacía"
+        (car lst))))
+
+; Definición de la función personalizada my-cdr para obtener el resto de una lista
+(define my-cdr
+  (lambda (lst)
+    (if (null? lst)
+        "La lista está vacía"
+        (cdr lst))))
 
 
 ;;6
@@ -171,41 +173,46 @@
 ;;cartesian-product
 ;; 7.
 ;;Proposito
-;; lst1 x lst2 x[L1]x[L2] -> [L1][L2]  La funci�n cartesian-product calcula el producto cartesiano entre dos listas, L1 y L2, y devuelve una
+;;[L1]x[L2] -> [L1][L2]  La funcion cartesian-product calcula el producto cartesiano entre dos listas, L1 y L2, y devuelve una
 ;; lista de pares ordenados que representan todas las combinaciones posibles de elementos de ambas listas.
 ;;
-;;Gram�tica BNF:
+;;Gramatica BNF:
 ;;<lista> :=()
 ;;         |(<valor-de-scheme> <lista>)
 ;;
 ;;Pruebas
-;;(display "(cartesian-product '(a b c) '(x y)): ")
-;;(display (cartesian-product '(a b c) '(x y))) ; Deber�a imprimir ((a x) (a y) (b x) (b y) (c x) (c y))
-;;(display "(cartesian-product '(p q r) '(5 6 7)): ")
-;;(display (cartesian-product '(p q r) '(5 6 7))) ; Deber�a imprimir ((p 5) (p 6) (p 7) (q 5) (q 6) (q 7) (r 5) (r 6) (r 7))
+;;(cartesian-product '(a b c) '(x y));Deberia imprimir ((a x) (a y) (b x) (b y) (c x) (c y))
+;;(cartesian-product '(p q r) '(5 6 7)); Deberia imprimir ((p 5) (p 6) (p 7) (q 5) (q 6) (q 7) (r 5) (r 6) (r 7))
+;;(cartesian-product '(1 2 3) '(5 4 3 7 8)); Deberia imprimir ((1 5) (1 4) (1 3) (1 7) (1 8) (2 5) (2 4) (2 3) (2 7) (2 8) (3 5) (3 4) (3 3) (3 7) (3 8))
+;;(cartesian-product '(0 1) '(2 y)); Deberia imprimir ((0 2) (0 y) (1 2) (1 y))
 
-(define (cartesian-product L1 L2)
-  ; Funci�n auxiliar para calcular el producto cartesiano entre dos listas
-  (define (product L1 L2)
-    (cond
-      [(null? L1) '()] ; Si la primera lista est� vac�a, no hay pares que formar
-      [else
-       (append-list (cartesian-pairs (car L1) L2) (product (cdr L1) L2))]))
+(define cartesian-product
+  (lambda (L1 L2)
+    ; Función auxiliar para calcular el producto cartesiano entre dos listas
+    (define product
+      (lambda (L1 L2)
+        (cond
+          [(null? L1) '()] ; Si la primera lista está vacía, no hay pares que formar
+          [else
+           (append-list (cartesian-pairs (car L1) L2) (product (cdr L1) L2))])))
 
-  ; Funci�n auxiliar para crear pares entre un elemento de L1 y todos los elementos de L2
-  (define (cartesian-pairs x L2)
-    (cond
-      [(null? L2) '()] ; Si la segunda lista est� vac�a, no hay m�s pares que formar
-      [else
-       (cons (list x (car L2)) (cartesian-pairs x (cdr L2)))]))
+    ; Función auxiliar para crear pares entre un elemento de L1 y todos los elementos de L2
+    (define cartesian-pairs
+      (lambda (x L2)
+        (cond
+          [(null? L2) '()] ; Si la segunda lista está vacía, no hay más pares que formar
+          [else
+           (cons (list x (car L2)) (cartesian-pairs x (cdr L2)))])))
 
-  ; Funci�n auxiliar para concatenar listas
-  (define (append-list lst1 lst2)
-    (if (null? lst1)
-        lst2
-        (cons (car lst1) (append-list (cdr lst1) lst2))))
+    ; Función auxiliar para concatenar listas
+    (define append-list
+      (lambda (lst1 lst2)
+        (if (null? lst1)
+            lst2
+            (cons (car lst1) (append-list (cdr lst1) lst2)))))
 
-  (product L1 L2)) ; Llama a la funci�n auxiliar product con las dos listas
+    (product L1 L2))) ; Llama a la función auxiliar product con las dos listas
+
 
 
 ;;9
@@ -276,42 +283,39 @@
 ;;up
 ;; 10.
 ;;Proposito
-;; [L]->[L]: La funci�n up toma una lista L y devuelve una nueva lista donde se han eliminado los par�ntesis del nivel m�s alto, es decir,
-;;quita los par�ntesis que rodean a los elementos en el nivel superior de la lista. 
+;; [L]->[L]: La funcion up toma una lista L y devuelve una nueva lista donde se han eliminado los parentesis del nivel mas alto, es decir,
+;;quita los parentesis que rodean a los elementos en el nivel superior de la lista. 
 ;; 
-;;Gram�tica BNF:
+;;Gramatica BNF:
 ;;<lista> :=()
 ;;         |(<valor-de-scheme> <lista>)
 ;;
 ;;Pruebas
-;;(display (up '((1 2) (3 4)))) ; Deber�a imprimir (1 2 3 4)
-;;(display (up '((x (y)) z))) ; Deber�a imprimir (x (y) z)
-;;(display (up '((x (12345) (y)) z))) ;Deber�a imprimir (x 12345 y z)
-;;(display (up '(a '(5*8/3))));Deber�a imprimir (a quote 5*8/3)
+;;(up '((1 2) (3 4)));Deberia imprimir (1 2 3 4)
+;;(up '((x (y)) z)); Deberia imprimir (x y z)
+;;(up '(z (z y) a)); Deberia imprimir (z z y a)
+;;(up '(1 (a b) 3)); Deberia imprimir (up '(1 (a b) 3))
 
+(define up
+  (lambda (L)
+    ; Función auxiliar para procesar un elemento del nivel más alto de la lista.
+    (define procesar-elemento
+      (lambda (elemento)
+        (cond
+          [(list? elemento) ; Si el elemento es una lista
+           (procesar-lista elemento)] ; Llamar a procesar-lista.
+          [else (list elemento)]))) ; Si el elemento no es una lista, incluirlo tal cual en una lista.
 
+    ; Función auxiliar para procesar una lista y aplicar procesar-elemento a cada elemento.
+    (define procesar-lista
+      (lambda (lista)
+        (cond
+          [(null? lista) '()] ; Si la lista está vacía, devuelve una lista vacía.
+          [else
+           (append (procesar-elemento (car lista)) ; Agregar el resultado de procesar-elemento del primer elemento.
+                   (procesar-lista (cdr lista)))]))) ; Llamar recursivamente a procesar-lista con el resto de la lista.
 
-; up: Lista -> Lista
-; Definici�n de la funci�n up que recibe una lista L y devuelve una lista con par�ntesis removidos del nivel m�s alto.
-(define (up L)
-  ; procesar-elemento: Elemento -> Lista
-  ; Funci�n auxiliar que procesa un elemento del nivel m�s alto de la lista.
-  (define (procesar-elemento elemento)
-    (cond
-      [(list? elemento) ; Si el elemento es una lista
-       (procesar-lista elemento)] ; Llamar a procesar-lista.
-      [else (list elemento)])) ; Si el elemento no es una lista, incluirlo tal cual en una lista.
-
-  ; procesar-lista: Lista -> Lista
-  ; Funci�n auxiliar que procesa una lista y aplica procesar-elemento a cada elemento de la lista.
-  (define (procesar-lista lista)
-    (cond
-      [(null? lista) '()] ; Si la lista est� vac�a, devuelve una lista vac�a.
-      [else
-       (append (procesar-elemento (car lista)) ; Agregar el resultado de procesar-elemento del primer elemento.
-               (procesar-lista (cdr lista)))])) ; Llamar recursivamente a procesar-lista con el resto de la lista.
-
-  (procesar-lista L)) ; Procesa la lista completa.
+    (procesar-lista L))) ; Procesa la lista completa.
 
 
 ;; 12
@@ -375,23 +379,30 @@
 ;;         |(<valor-de-scheme> <lista>)
 ;;
 ;;Pruebas
-;;(display (operate (list + * + - *) '(1 2 8 4 11 6))) ; Debe imprimir 102
-;;(display (operate (list *) '(4 5))) ; Debe imprimir 20
-;;(display (operate (list - / +) '(10 2 5 3))) ; Debe imprimir 0
-;;(display (operate (list * + - /) '(3 2 4 2 2))) ; Debe imprimir 6
+;;(operate (list + * + - *) '(1 2 8 4 11 6)); Debe imprimir 102
+;;(operate (list *) '(4 5)); Debe imprimir 20
+;;(operate (list * -) '(4 4 2)); Debe imprimir 14
+;;(operate (list + - * *) '(1 1 1 3 9)); Debe imprimir 27
 
-(define (operate lrators lrands)
-  (define (apply-operator operador operando1 operando2)
-    (operador operando1 operando2))
+(define operate
+  (lambda (lrators lrands)
+  
+  (define apply-operator
+    (lambda (operador operando1 operando2)
+    (operador operando1 operando2)))
 
-  (define (apply-operators operadores operandos)
+  (define apply-operators
+    (lambda (operadores operandos)
     (if (null? operadores)
         (car operandos)
         (apply-operators (cdr operadores)
                          (cons (apply-operator (car operadores) (car operandos) (cadr operandos))
-                               (cddr operandos)))))
+                               (cddr operandos))))))
 
-  (apply-operators lrators lrands))
+  (apply-operators lrators lrands)
+
+  ))
+
 
 ;;15
 ;; count-odd-and-even :
@@ -429,7 +440,7 @@
 ;;Proposito
 ;; (operador)x(operando1)x(operando2)->(operacionB):La funci�n operate realiza una serie de c�lculos binarios sucesivos seg�n los operadores y operandos proporcionados.
 ;; 
-;;Gram�tica BNF:
+;;Gramatica BNF:
 ;;<OperacionB>::= <int>
 ;;::= (<OperacionB> �suma <OperacionB>)
 ;;::= (<OperacionB> �resta <OperacionB>)
@@ -437,19 +448,22 @@
 ;;
 ;;
 ;;Pruebas
-;;(display (Operar-binarias 4)) ; Debe imprimir 4
-;;(display (Operar-binarias '(2 suma 9))) ; Debe imprimir 11
-;;(display (Operar-binarias '(2 resta 9))) ; Debe imprimir -7
-;;(display (Operar-binarias '(2 multiplica 9))) ; Debe imprimir 18
-;;(display (Operar-binarias '(6 resta (2 suma (3 multiplica 4)))));Debe imprimir -8
-;;(display (Operar-binarias '(3 resta (4 suma 2)))) ; Debe imprimir -3
-;;(display (Operar-binarias '((2 multiplica 3) suma (5 resta 1))))
-;;(display (Operar-binarias '((2 multiplica (4 suma 1))multiplica((2 multiplica 4) resta 1))))
+;;Operar-binarias 4); Deberia imprimir 4
+;;(Operar-binarias '(2 suma 9) );Deberia imprimir 11
+;;(Operar-binarias '(2 resta 9) );Deberia imprimir -7
+;;(Operar-binarias '(2 multiplica 9) );Deberia imprimir 18
+;;(Operar-binarias '( (2 multiplica 3) suma (5 resta 1 ) ) );Deberia imprimir 10
+;;(Operar-binarias '( (2 multiplica (4 suma 1) )
+;;multiplica
+;;( (2 multiplica 4) resta 1 ) ) );Deberia imprimir 70
+;;(Operar-binarias '(1 suma 4));Deberia imprimir 5
+;;(Operar-binarias '(1 resta 205));Deberia imprimir -204
+
 
 
 (define (Operar-binarias operacionB)
   (cond
-    ((number? operacionB) operacionB) ; Caso base: si es un n�mero, se retorna el n�mero
+    ((number? operacionB) operacionB) ; Caso base: si es un numero, se retorna el numero
     ((list? operacionB)
      (let* ((operador (cadr operacionB))
             (operando1 (Operar-binarias (car operacionB)))
@@ -458,8 +472,8 @@
          ((eq? operador 'suma) (+ operando1 operando2))
          ((eq? operador 'resta) (- operando1 operando2))
          ((eq? operador 'multiplica) (* operando1 operando2))
-         (else ("Operador no v�lido")))))
-    (else ("Operaci�n binaria no v�lida"))))
+         (else ("Operador no valido")))))
+    (else ("Operacion binaria no valida"))))
 
 ;;18
 ;; factorial :
@@ -548,47 +562,3 @@
             (cons (pascal-element row col)
                   (generate-col (+ col 1))))))
     (generate-col 1)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
